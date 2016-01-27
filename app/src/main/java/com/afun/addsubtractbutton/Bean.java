acee8825ffd24901dda67f3e6619b48356c07203
wangqingxi <794358253@qq.com>
2016-01-27 14:01:55 +0800

    test change

--- app/src/main/java/com/afun/addsubtractbutton/Bean.java
+++ app/src/main/java/com/afun/addsubtractbutton/Bean.java
@@ -4,6 +4,6 @@ package com.afun.addsubtractbutton;
  * Created by qingxi.wang on 2016/1/27.
  */
 public class Bean {
-    public String show = "1";
+    public String show = "1.0";
     public float count = (float) 1.0;
 }
